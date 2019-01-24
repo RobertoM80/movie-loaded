6402a1c16232382406e18072066c6243bc9d0f32
Roberto Mirabella <bronzetto@outlook.com>
2019-01-24 10:03:41 +0000

    removed redux web tool for production

--- src/config/store.js
+++ src/config/store.js
@@ -1,31 +1,30 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import thunk from 'redux-thunk';
-import weatherReducer from './reducers/weatherReducer';
-import isLoadingReducer from './reducers/isLoadingReducer';
-import chuckNorrisReducer from './reducers/chuckNorrisReducer';
-import moviesReducer from './reducers/moviesReducer';
-import overlayReducer from './reducers/overlayReducer';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+import weatherReducer from "./reducers/weatherReducer";
+import isLoadingReducer from "./reducers/isLoadingReducer";
+import chuckNorrisReducer from "./reducers/chuckNorrisReducer";
+import moviesReducer from "./reducers/moviesReducer";
+import overlayReducer from "./reducers/overlayReducer";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const rootReducer = combineReducers({
-    weather: weatherReducer,
-    isLoading: isLoadingReducer,
-    chuckNorris: chuckNorrisReducer,
-    movies: moviesReducer,
-    overlay: overlayReducer
-
+  weather: weatherReducer,
+  isLoading: isLoadingReducer,
+  chuckNorris: chuckNorrisReducer,
+  movies: moviesReducer,
+  overlay: overlayReducer
 });
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
